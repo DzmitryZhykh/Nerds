b5cbf775e97664754aede1c69196c03be3c9600c
naryzhnaj <naryzhnaj@gmail.com>
2018-04-06 23:10:39 +0300

    итоговая версия

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,25 @@
 var open = document.querySelector(".write-us");
 var popup = document.querySelector(".modal");
 var close = document.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var username = form.querySelector("[name=name]");
+var email = form.querySelector("[name=email]");
 
 open.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.add("modal-show");
+	username.focus();
 });
 
 close.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!username.value || !email.value) {
+		evt.preventDefault();
+		popup.classList.add("modal-error");
+	} 
 });
